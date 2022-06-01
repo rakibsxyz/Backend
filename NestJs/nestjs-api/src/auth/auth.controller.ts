8b08aeb900d42b637df5fa03998c73f0878abaa8
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2022-06-01 18:09:53 +0000

    sign in logic added

--- NestJs/nestjs-api/src/auth/auth.controller.ts
+++ NestJs/nestjs-api/src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
     }
 
     @Post('signin')
-    signin(){
-       return this.authService.login()
+    signin(@Body() dto: AuthDto){
+       return this.authService.login(dto)
     }
 }
